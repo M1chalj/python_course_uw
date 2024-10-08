17cee5d0dd41029b725d32ad60d09029b3487794
Michał Januszkiewicz <mjanuszkiewicz2004@gmail.com>
2024-10-08 23:11:18 +0200

    Rozwiązanie małego zadania 1

--- Małe zadanie 1/main.py
+++ Małe zadanie 1/main.py
@@ -2,18 +2,18 @@ import random
 import math
 
 
-def monte_carlo(n, k):
-    licznik = 0
+def pi_monte_carlo(n, k):
+    in_cnt = 0
     for i in range(n):
         x = random.random()
         y = random.random()
         if x ** 2 + y ** 2 <= 1:
-            licznik += 1
+            in_cnt += 1
 
         if (i + 1) % k == 0:
-            print("Krok", i + 1, "wartość:", licznik / (i + 1) * 4)
-    przyblizenie_pi = licznik / n * 4
-    print("Wartość po", n, "krokach:", przyblizenie_pi, "błąd:", abs(math.pi - przyblizenie_pi))
+            print("Krok", i + 1, "wartość:", in_cnt / (i + 1) * 4)
+    estimated_pi = in_cnt / n * 4
+    print("Wartość po", n, "krokach:", estimated_pi, "błąd:", abs(math.pi - estimated_pi))
 
 
-monte_carlo(int(input("Podaj liczbę iteracji: ")), int(input("Podaj krok: ")))
+pi_monte_carlo(int(input("Podaj liczbę iteracji: ")), int(input("Podaj krok: ")))
