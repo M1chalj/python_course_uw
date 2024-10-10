1c7db61732173bfe437660a631435d7a24e91ec7
michalj <mjanuszkiewicz2004@gmail.com>
2024-10-11 01:23:23 +0200

    Rozwiązanie małego zadania 2

---
+++ Małe zadanie 2/trudniejsze.py
@@ -0,0 +1,64 @@
+aktywnosci = {}
+
+
+def dodaj():
+    nazwa = input("Podaj nazwę: ")
+    czas = int(input("Podaj czas: "))
+    if nazwa not in aktywnosci.keys():
+        aktywnosci[nazwa] = []
+    aktywnosci[nazwa].append(czas)
+    print("Dodano")
+
+
+def sumuj_czas(aktywnosc):
+    czas = 0
+    for t in aktywnosci[aktywnosc]:
+        czas += t
+    return czas
+
+
+def pokaz_czas():
+    nazwa = input("Podaj nazwę: ")
+    if nazwa not in aktywnosci.keys():
+        print("Niepoprawna nazwa")
+    else:
+        czas = sumuj_czas(nazwa)
+        print("Całkowity czas:", czas)
+
+
+def pokaz_top():
+    ranking = []
+    for nazwa in aktywnosci.keys():
+        ranking.append((sumuj_czas(nazwa), nazwa))
+    ranking.sort(reverse=True)
+    for i in range(min(3, len(ranking))):
+        print(i + 1, ". ", ranking[i][1], ": ", ranking[i][0], sep="")
+
+
+def koniec():
+    exit(0)
+
+
+funkcje = {
+    "dod": dodaj,
+    "pok": pokaz_czas,
+    "top": pokaz_top,
+    "wyj": koniec
+}
+opisy = {
+    "dod": "Dodaj aktywność",
+    "pok": "Pokaż czas",
+    "top": "Pokaż top",
+    "wyj": "Wyjście"
+}
+
+while True:
+    print("Dostępne funkcje:")
+    for skrot, opis in zip(opisy.keys(), opisy.values()):
+        print(skrot, ": ", opis, sep="")
+
+    func = input("Co chcesz zrobić: ")
+    while func not in funkcje.keys():
+        func = input("Niepoprawny kod, spróbuj jeszcze raz: ")
+
+    funkcje[func]()
