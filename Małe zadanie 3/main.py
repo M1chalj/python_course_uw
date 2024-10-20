ae9d935fbe5840f1f61d61d3c167e22db8cd9209
michalj <mjanuszkiewicz2004@gmail.com>
2024-10-21 01:08:27 +0200

    Drobne poprawki

--- Małe zadanie 3/main.py
+++ Małe zadanie 3/main.py
@@ -5,69 +5,77 @@ import os
 import csv
 import json
 
+dni_tygodnia_skroty = {
+    'pn': 0,
+    'wt': 1,
+    'sr': 2,
+    'cz': 3,
+    'pt': 4,
+    'sb': 5,
+    'nd': 6
+}
+
+dni_tygodnia_nazwy = {
+    0: 'poniedziałek',
+    1: 'wtorek',
+    2: 'środa',
+    3: 'czwartek',
+    4: 'piątek',
+    5: 'sobota',
+    6: 'niedziela'
+}
+
+pory_dnia_nazwy = {
+    'r': 'rano',
+    'w': 'wieczorem'
+}
+
+modele = ['A', 'B', 'C']
+
+domyslna_pora_dnia = 'rano'
+
 
 def wczytaj_argumenty():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-miesiace",
-                        help="lista miesięcy",
+    parser.add_argument('-miesiace',
+                        help='lista miesięcy',
                         nargs='+',
                         required=True)
-    parser.add_argument("-dni",
-                        help="lista dni lub przedziałów dla każdego miesiąca np. pn-wt, pt",
+    parser.add_argument('-dni',
+                        help='lista dni lub przedziałów dla każdego miesiąca np. pn-wt, pt',
                         nargs='+',
                         required=True)
-    parser.add_argument("-pory",
-                        help="pory dnia (rano(r)/wieczór(w)) dla każdej kombinacji miesiąc - dzień. Domyślnie rano",
+    parser.add_argument('-pory',
+                        help='pory dnia (rano(r)/wieczór(w)) dla każdej kombinacji miesiąc - dzień. Domyślnie rano',
                         nargs='+',
                         required=True)
-    parser.add_argument("-t",
-                        help="tworzenie plików (domyślnie pliki są odczytywane)",
-                        action="store_const",
-                        const="t",
-                        default="o",
-                        dest="tryb")
-    parser.add_argument("-j",
-                        help="przetwarza pliki o rozszerzeniu json",
-                        action="store_true",
-                        dest="json")
-    parser.add_argument("-c",
-                        help="przetwarza pliki o rozszerzeniu csv",
-                        action="store_true",
-                        dest="csv")
+    parser.add_argument('-t',
+                        help='tworzenie plików (domyślnie pliki są odczytywane)',
+                        action='store_const',
+                        const='t',
+                        default='o',
+                        dest='tryb')
+    parser.add_argument('-j',
+                        help='przetwarza pliki o rozszerzeniu json',
+                        action='store_true',
+                        dest='json')
+    parser.add_argument('-c',
+                        help='przetwarza pliki o rozszerzeniu csv',
+                        action='store_true',
+                        dest='csv')
     args = parser.parse_args()
 
     if not args.json and not args.csv:
         parser.print_help()
-        print("Nie podano żadnego formatu plików")
+        print('Nie podano żadnego formatu plików')
         exit()
 
     if len(args.miesiace) != len(args.dni):
         parser.print_help()
-        print("Liczba miesięcy i dni jest różna")
+        print('Liczba miesięcy i dni jest różna')
         exit()
 
     return args
-
-
-dni_tygodnia_skroty = {
-    "pn": 0,
-    "wt": 1,
-    "sr": 2,
-    "cz": 3,
-    "pt": 4,
-    "sb": 5,
-    "nd": 6
-}
-
-dni_tygodnia_nazwy = {
-    0: "poniedziałek",
-    1: "wtorek",
-    2: "środa",
-    3: "czwartek",
-    4: "piątek",
-    5: "sobota",
-    6: "niedziela"
-}
 
 
 def daj_liste_dni(zakres):
@@ -75,7 +83,7 @@ def daj_liste_dni(zakres):
     if '-' in zakres:
         lista = zakres.split('-')
         if len(lista) != 2 or lista[0] not in dni_tygodnia_skroty or lista[1] not in dni_tygodnia_skroty:
-            print("niepoprawny zakres dni tygodnia")
+            print('niepoprawny zakres dni tygodnia')
             exit()
 
         for i in range(dni_tygodnia_skroty[lista[0]], dni_tygodnia_skroty[lista[1]] + 1):
@@ -85,7 +93,7 @@ def daj_liste_dni(zakres):
         if zakres in dni_tygodnia_skroty:
             wynik.append(dni_tygodnia_nazwy[dni_tygodnia_skroty[zakres]])
         else:
-            print("niepoprawny zakres dni tygodnia")
+            print('niepoprawny zakres dni tygodnia')
             exit()
     return wynik
 
@@ -97,12 +105,13 @@ def przetworz_listy(miesiace, dni, pory):
         lista_dni = daj_liste_dni(dni[i])
         for dzien in lista_dni:
             if indeks_pora < len(pory):
-                if pory[indeks_pora] == 'r':
-                    pora = 'rano'
+                if not pory[indeks_pora] in pory_dnia_nazwy:
+                    print('niepoprawna pora')
+                    exit()
                 else:
-                    pora = 'wieczorem'
+                    pora = pory_dnia_nazwy[pory[indeks_pora]]
             else:
-                pora = 'rano'
+                pora = domyslna_pora_dnia
             skladowe_sciezek.append([miesiace[i], dzien, pora])
             indeks_pora += 1
     return skladowe_sciezek
@@ -121,13 +130,10 @@ def zrob_sciezki(skladowe_sciezek):
     return sciezki
 
 
-modele = ["A", "B", "C"]
-
-
 def zapis_do_csv(sciezki):
     for sciezka in sciezki:
-        dane = [["Model", "Wynik", "Czas"],
-                [random.choice(modele), random.randint(0, 1000), str(random.randint(0, 1000)) + "s"]]
+        dane = [['Model', 'Wynik', 'Czas'],
+                [random.choice(modele), random.randint(0, 1000), str(random.randint(0, 1000)) + 's']]
         with open(os.path.join(sciezka, 'Dane.csv'), 'w', newline='') as plik:
             pisarz = csv.writer(plik, delimiter=';')
             for wiersz in dane:
@@ -136,9 +142,9 @@ def zapis_do_csv(sciezki):
 
 def zapis_do_json(sciezki):
     for sciezka in sciezki:
-        dane = {"Model": random.choice(modele),
-                "Wynik": random.randint(0, 1000),
-                "Czas": str(random.randint(0, 1000)) + "s"}
+        dane = {'Model': random.choice(modele),
+                'Wynik': random.randint(0, 1000),
+                'Czas': str(random.randint(0, 1000)) + 's'}
         with open(os.path.join(sciezka, 'Dane.json'), 'w') as plik:
             json.dump(dane, plik)
 
@@ -154,11 +160,11 @@ def odczyt_z_csv(sciezki, model):
         with open(sciezka_do_pliku, 'r') as plik:
             czytelnik = csv.reader(plik, delimiter=';')
             naglowki = next(czytelnik)
-            if "Model" not in naglowki or "Czas" not in naglowki:
-                print("Błędny plik", os.path.join(sciezka, 'Dane.csv'))
+            if 'Model' not in naglowki or 'Czas' not in naglowki:
+                print('Błędny plik', sciezka_do_pliku)
                 continue
-            kolumnaModel = naglowki.index("Model")
-            kolumnaCzas = naglowki.index("Czas")
+            kolumnaModel = naglowki.index('Model')
+            kolumnaCzas = naglowki.index('Czas')
             for wiersz in czytelnik:
                 if wiersz[kolumnaModel] == model:
                     suma += int(wiersz[kolumnaCzas][:-1])
@@ -173,8 +179,11 @@ def odczyt_z_json(sciezki, model):
             continue
         with open(sciezka_do_pliku, 'r') as plik:
             dane = json.load(plik)
-            if (dane["Model"] == model):
-                suma += int(dane["Czas"][:-1])
+            if not ('Model' in dane or 'Czas' in dane):
+                print('Błędny plik', sciezka_do_pliku)
+                continue
+            if (dane['Model'] == model):
+                suma += int(dane['Czas'][:-1])
     return suma
 
 
@@ -188,7 +197,7 @@ if args.tryb == 't':
 else:
     suma = 0
     if args.json:
-        suma += odczyt_z_json(sciezki, "A")
+        suma += odczyt_z_json(sciezki, 'A')
     if args.csv:
-        suma += odczyt_z_csv(sciezki, "A")
-    print("Suma czasów:", suma)
+        suma += odczyt_z_csv(sciezki, 'A')
+    print('Suma czasów:', suma)
