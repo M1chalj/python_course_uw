3dbbb5a931b821d53d8eb137352f3b62af88f2d4
michalj <mjanuszkiewicz2004@gmail.com>
2024-11-05 01:03:29 +0100

    Małe zadanie 5

---
+++ Małe zadanie 5/main.py
@@ -0,0 +1,76 @@
+import math
+
+
+class Ułamek:
+    def __init__(self, licznik, mianownik):
+        assert mianownik != 0;
+        self.__licznik = licznik
+        self.__mianownik = mianownik
+        self.skróć()
+
+    def skróć(self):
+        gcd = math.gcd(self.__licznik, self.__mianownik)
+        self.__licznik //= gcd
+        self.__mianownik //= gcd
+
+    def __add__(self, other):
+        wynik = Ułamek(self.__licznik * other.__mianownik + self.__mianownik * other.__licznik,
+                       self.__mianownik * other.__mianownik)
+        wynik.skróć()
+        return wynik
+
+    def __sub__(self, other):
+        wynik = Ułamek(self.__licznik * other.__mianownik - self.__mianownik * other.__licznik,
+                       self.__mianownik * other.__mianownik)
+        wynik.skróć()
+        return wynik
+
+    def __mul__(self, other):
+        wynik = Ułamek(self.__licznik * other.__licznik, self.__mianownik * other.__mianownik)
+        wynik.skróć()
+        return wynik
+
+    def __truediv__(self, other):
+        wynik = Ułamek(self.__licznik * other.__mianownik, self.__mianownik * other.__licznik)
+        wynik.skróć()
+        return wynik
+
+    def __eq__(self, other):
+        return self.__licznik == other.__licznik and self.__mianownik == other.__mianownik
+
+    def __gt__(self, other):
+        return self.__licznik * other.__mianownik > other.__licznik * self.__mianownik
+
+    def __ge__(self, other):
+        return self.__licznik * other.__mianownik >= other.__licznik * self.__mianownik
+
+    def __str__(self):
+        return str(self.__licznik) + '/' + str(self.__mianownik)
+
+    def __repr__(self):
+        return str(self)
+
+
+połowa = Ułamek(1, 2)
+ćwierć = Ułamek(1, 4)
+setna = Ułamek(1, 100)
+
+print(połowa, '+', ćwierć, '=', połowa + ćwierć)
+assert połowa + ćwierć == Ułamek(3, 4)
+
+print(połowa, '-', ćwierć, '=', połowa - ćwierć)
+assert połowa - ćwierć == ćwierć
+
+print(setna, '*', ćwierć, '=', setna * ćwierć)
+assert setna * ćwierć == Ułamek(1, 400)
+
+print(setna, '/', ćwierć, '=', setna / ćwierć)
+assert setna / ćwierć == Ułamek(4, 100)
+
+assert setna < połowa
+assert ćwierć < połowa
+assert setna < ćwierć
+assert połowa <= połowa
+assert setna <= połowa
+
+# błąd = Ułamek(10, 0)
