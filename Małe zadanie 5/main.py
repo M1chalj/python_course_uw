a9ed75311b8e747b64e97ae6663aa197f34a7490
michalj <mjanuszkiewicz2004@gmail.com>
2024-11-06 22:12:27 +0100

    Fixing bugs

--- Małe zadanie 5/main.py
+++ Małe zadanie 5/main.py
@@ -3,7 +3,8 @@ import math
 
 class Ułamek:
     def __init__(self, licznik, mianownik):
-        assert mianownik != 0;
+        assert mianownik != 0
+
         self.__licznik = licznik
         self.__mianownik = mianownik
         self.skróć()
@@ -12,6 +13,13 @@ class Ułamek:
         gcd = math.gcd(self.__licznik, self.__mianownik)
         self.__licznik //= gcd
         self.__mianownik //= gcd
+
+        if self.__mianownik < 0:
+            self.__licznik *= -1
+            self.__mianownik *= -1
+
+        if self.__licznik == 0:
+            self.__mianownik = 1
 
     def __add__(self, other):
         wynik = Ułamek(self.__licznik * other.__mianownik + self.__mianownik * other.__licznik,
@@ -54,6 +62,8 @@ class Ułamek:
 połowa = Ułamek(1, 2)
 ćwierć = Ułamek(1, 4)
 setna = Ułamek(1, 100)
+zero = Ułamek(0,10)
+zero2 = Ułamek(0, 17)
 
 print(połowa, '+', ćwierć, '=', połowa + ćwierć)
 assert połowa + ćwierć == Ułamek(3, 4)
@@ -67,10 +77,14 @@ assert setna * ćwierć == Ułamek(1, 400)
 print(setna, '/', ćwierć, '=', setna / ćwierć)
 assert setna / ćwierć == Ułamek(4, 100)
 
+print(ćwierć, '-', połowa, '=', ćwierć - połowa)
+assert ćwierć - połowa == zero - ćwierć
+
 assert setna < połowa
 assert ćwierć < połowa
 assert setna < ćwierć
 assert połowa <= połowa
 assert setna <= połowa
+assert zero == zero2
 
 # błąd = Ułamek(10, 0)
